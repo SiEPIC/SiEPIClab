734a281e31752b0113295baeeae5a4748723e773
IDA <jonathanyorkbarnes@gmail.com>
2022-06-27 14:59:48 -0700

    [PY3] fixed fine align

--- pyOptomip/fineAlign.py
+++ pyOptomip/fineAlign.py
@@ -46,12 +46,6 @@ class fineAlign(object):
     
     useCrosshair = 0 # Set to 1 to use crosshair search after gradient. Doesn't work very well.
 
-    useDebugStep = 0 #set to 1 to turn on tracking of power and steps
-    
-    useFineGradient = True; #Secondary Gradient Search
-    
-    UltraFineAlign = True; #Sets pwm avging time to 0.01 sec. Accuracy and scan time are increased
-    
     abort = False # Can set to true to self.abort a fine align
     
     NO_ERROR = 0
@@ -67,15 +61,7 @@ class fineAlign(object):
     def doFineAlign(self):
         xStartPos = self.stage.getPosition()[0];
         yStartPos = self.stage.getPosition()[1];
-        
-        ##Debug##
-        if self.useDebugStep == 1:
-            #Logfile Section## Another Section below as well##
-            self.logfileSpiral = open('Logfile_Spiral'+str(self.stepSize)+'um.txt','w+')#For Analytical Purposes 
-            self.logfileRGrad = open('Logfile_RGrad'+str(self.stepSize)+'um.txt','w+')
-            if self.useFineGradient == True:
-                self.logfileFGrad = open('Logfile_FGrad'+str(self.stepSize)+'um.txt','w+')
-        #/Debug#
+
                 
         for det in self.detectorPriority:
             maxSteps = math.ceil(self.scanWindowSize/float(self.stepSize))
@@ -87,12 +73,6 @@ class fineAlign(object):
             self.laser.setPWMPowerRange(detSlot, detChan, 'auto', 0)
             # Try to set laser output. If the laser only has one output, an error is thrown
             # which will be ignored here
-            
-            if self.UltraFineAlign == True:
-                self.laser.setPWMAveragingTime(detSlot,detChan,(0.01))
-            else:
-                self.laser.setPWMAveragingTime(detSlot,detChan,(0.0001))
-            #Sets the PWMAveraging Time    
             
             try:
                 self.laser.setTLSOutput(self.laserOutput, slot=self.laserSlot)
@@ -117,23 +97,13 @@ class fineAlign(object):
             print ('Found a device. Optimizing power...')   
             
             # Gradient search stage      
-            res = self.gradientSearchRough(detSlot, detChan)
-            
-            if self.useFineGradient == True:
-                res = self.gradientSearchFine(detSlot, detChan)
+            res = self.gradientSearch(detSlot, detChan)
               
             # Crosshair method
             if self.useCrosshair:
                 res = self.crosshairSearch(maxSteps, detSlot, detChan)
             self.laser.setAutorangeAll()
             print ('Fine align completed.')
-            #Debug#
-            if self.useDebugStep ==1:
-                self.logfileSpiral.close() 
-                self.logfileRGrad.close()
-                if self.useFineGradient == True:
-                    self.logfileFGrad.close()
-            #/Debug#
             return res
             
         # Fine align failed  
@@ -166,9 +136,6 @@ class fineAlign(object):
                     return self.FINE_ALIGN_ABORTED
                 elif power > self.threshold:
                     return self.NO_ERROR
-                    if self.useDebugStep == 1:
-                        self.logfileSpiral.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-   
                     
             # Y movement
             for ii in range(1, numSteps+1):
@@ -178,9 +145,7 @@ class fineAlign(object):
                     return self.FINE_ALIGN_ABORTED
                 elif power > self.threshold:
                     return self.NO_ERROR
-                    if self.useDebugStep == 1:
-                        self.logfileSpiral.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                    
+
             numSteps += 1
             
             # Swap sweep direction
@@ -193,7 +158,7 @@ class fineAlign(object):
         return self.DEVICE_NOT_FOUND # Return error    
             
             
-    def gradientSearchRough(self, detSlot, detChan):
+    def gradientSearch(self, detSlot, detChan):
         peakFoundCount = 0; # Count how many consective peaks are found
         numConsecutivePeaks = 1; # Need this many consecutive peaks to conclude the peak was found  
         for ii in range(self.numGradientIter):
@@ -207,10 +172,6 @@ class fineAlign(object):
             
             if power_posx > power:
                 peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileRGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
                 continue
             
             self.stage.moveRelative(-2*self.stepSize,0)
@@ -219,10 +180,6 @@ class fineAlign(object):
                     
             if power_negx > power:
                 peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileRGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
                 continue
             
             self.stage.moveRelative(self.stepSize,self.stepSize)
@@ -231,10 +188,6 @@ class fineAlign(object):
                     
             if power_posy > power:
                 peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileRGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
                 continue
             
             self.stage.moveRelative(0,-2*self.stepSize)
@@ -243,178 +196,16 @@ class fineAlign(object):
             
             if power_negy > power:
                 peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileRGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
+
                 continue
             
-            
-         #8 Axis#   
-            #XX
-            self.stage.moveRelative(self.stepSize,0)
-            power_xnegy = self.laser.readPWM(detSlot, detChan)
-                      
-            if power_xnegy > power:
-                peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileRGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
-                continue
-            #XY
-            self.stage.moveRelative(0,2*self.stepSize)            
-            power_xy = self.laser.readPWM(detSlot, detChan)
-                      
-            if power_xy > power:
-                peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileRGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
-                continue
-            #negXY
-            self.stage.moveRelative(-2*self.stepSize,0)
-            power_negxy = self.laser.readPWM(detSlot, detChan)
-                      
-            if power_negxy > power:
-                peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileRGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
-                continue
-            
-            #negXnegY
-            self.stage.moveRelative(0,-2*self.stepSize)
-            power_negxnegy = self.laser.readPWM(detSlot, detChan)
-                      
-            if power_negxnegy > power:
-                peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileRGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
-                continue
-            
-            self.stage.moveRelative(self.stepSize,self.stepSize)
+
+            self.stage.moveRelative(0,self.stepSize)
             if peakFoundCount == numConsecutivePeaks:
                 return self.NO_ERROR
             
             peakFoundCount += 1
 
-            
-        return self.NO_ERROR
-        
-    def gradientSearchFine(self, detSlot, detChan):
-        peakFoundCount = 0; # Count how many consective peaks are found
-        numConsecutivePeaks = 1; # Need this many consecutive peaks to conclude the peak was found  
-        for ii in range(self.numGradientIter):
-            if self.abort:
-                return self.FINE_ALIGN_ABORTED
-            # Always move in the direction of increasing power
-            power = self.laser.readPWM(detSlot, detChan)
-            
-            self.stage.moveRelative(0.5,0)
-            power_posx = self.laser.readPWM(detSlot, detChan)
-            
-            if power_posx > power:
-                peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileFGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
-                continue
-            
-            self.stage.moveRelative(-2*0.5,0)
-            power_negx = self.laser.readPWM(detSlot, detChan)
-
-                    
-            if power_negx > power:
-                peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileFGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
-                continue
-            
-            self.stage.moveRelative(0.5,0.5)
-            power_posy = self.laser.readPWM(detSlot, detChan)
-                     
-                    
-            if power_posy > power:
-                peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileFGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
-                continue
-            
-            self.stage.moveRelative(0,-2*0.5)
-            power_negy = self.laser.readPWM(detSlot, detChan)
-                      
-            
-            if power_negy > power:
-                peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileFGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
-                continue
-            
-            
-         #8 axis#   
-            #XX
-            self.stage.moveRelative(0.5,0)
-            power_xnegy = self.laser.readPWM(detSlot, detChan)
-                      
-            if power_xnegy > power:
-                peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileFGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
-                continue
-            #XY
-            self.stage.moveRelative(0,2*0.5)
-            power_xy = self.laser.readPWM(detSlot, detChan)
-                      
-            if power_xy > power:
-                peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileFGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
-                continue
-            #negXY
-            self.stage.moveRelative(-2*0.5,0)
-            power_negxy = self.laser.readPWM(detSlot, detChan)
-                      
-            if power_negxy > power:
-                peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileFGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
-                continue
-            
-            #negXnegY
-            self.stage.moveRelative(0,-2*0.5)
-            power_negxnegy = self.laser.readPWM(detSlot, detChan)
-                      
-            if power_negxnegy > power:
-                peakFoundCount = 0
-                #Debug
-                if self.useDebugStep == 1:
-                   self.logfileFGrad.write(str(self.stage.getPosition()[0])+","+str(self.stage.getPosition()[1])+","+str(power)+"\n")
-                 #
-                continue
-            
-            self.stage.moveRelative(0.5,0.5)
-            if peakFoundCount == numConsecutivePeaks:
-                return 0
-            
-            peakFoundCount += 1
 
             
         return self.NO_ERROR
