1acc65dc6d5185b3b505949b7575c85b0dd7bd43
Maddy561 <68255880+Maddy561@users.noreply.github.com>
2022-12-14 15:50:58 -0800

    check to make sure devices are selected before performing measurements

--- pyOptomip/autoMeasurePanel.py
+++ pyOptomip/autoMeasurePanel.py
@@ -1129,6 +1129,9 @@ class autoMeasurePanel(wx.Panel):
             if not activeDetectors:
                 print("Please Select a Detector.")
 
+            elif not checkedDevicesText:
+                print("Please Select Devices to Measure.")
+
             else:
 
                 # Start measurement using the autoMeasure device
